f1ff20e500ffc81001c6b02316d6aa360802fc4f
PinedaVictor <pinedavictor095@gmail.com>
2022-06-07 18:21:43 -0700

    Updated metals project properties

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,10 @@
-
 import user.User;
 import user.HairColor;
+import data.TestingData;
 
 @main def main = {
-    println("In the main funciton");
-    val n = new User(27, "Vic", "P", HairColor.Brown, List("Ringo"));
-    println(n.getFullName());
-    
+  println("In the main funciton");
+  val n = new User(27, "Vic", "P", HairColor.Brown, List("Ringo"));
+  println(n.getFullName());
+
 }
--- src/main/scala/User.scala
+++ src/main/scala/User.scala
@@ -1,15 +1,19 @@
-
-package user
+package user;
 
 enum HairColor:
-    case Red, Blue, Brown, Black, Blonde
+  case Red, Blue, Brown, Black, Blonde
 
-
-class User(age: Int, firstName: String, lastName: String, hairColor: HairColor, pets: List[String]) {
-    def getAge() = age;
-    def getFullName() = s"${firstName} ${lastName}";
-    def getFirstName() = firstName;
-    def getLastName() = lastName;
-    def getHairColor() = hairColor;
-    def getPets() = pets;
+class User(
+    age: Int,
+    firstName: String,
+    lastName: String,
+    hairColor: HairColor,
+    pets: List[String]
+) {
+  def getAge() = age;
+  def getFullName() = s"${firstName} ${lastName}";
+  def getFirstName() = firstName;
+  def getLastName() = lastName;
+  def getHairColor() = hairColor;
+  def getPets() = pets;
 }
